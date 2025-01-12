5c8bbb5bfa5dc4799de7c62dcc27ed92f828f480
Abdulaziz_c5 <abdulazizbekabdusodiqov@gmail.com>
2025-01-12 15:32:16 +0500

    commit

--- controller/freelancer.controller.js
+++ controller/freelancer.controller.js
@@ -14,32 +14,62 @@ const createFreelancer = async (req, res) => {
         if(oldFrelancer?.dataValues){
             return res.status(400).send({message : "bu email bilan oldin ro'yxatdan o'tilgan"})
         }
-
+        
         const {error, value} = freelancerValidation(req.body)
-
+        
         if(error){
             errorHandler(error, res)
         }
-
+        
         const hashedPassword = bcrypt.hashSync(value.password, 7)
         const newFreelancer =  Freelancer.build({...value, password : hashedPassword})
-
+        
         const payload = {
             id: newFreelancer.id,
             name : newFreelancer.name,
             email : newFreelancer.email,
             role : "freelancer"
         }
-
+        
         const token = jwt.generateTokens(payload)
         
         newFreelancer.refresh_token = token.refreshToken
-
+        
         console.log(newFreelancer);
         
         await newFreelancer.save()
-
+        
         return res.send(newFreelancer)
+        
+    } catch (error) {
+        errorHandler(error, res)
+    }
+}
+
+
+const getFreelancers = async (req, res) => {
+    try {
+        
+        const freelancers = await Freelancer.findAll()
+        
+        return res.status(200).send(freelancers)
+        
+    } catch (error) {
+        errorHandler(error, res)
+    }
+}
+
+
+const getFreelancerById = async (req, res) => {
+    try {
+        
+        const freelancers = await Freelancer.findByPk(req.params.id)
+        
+        if(!freelancers?.dataValues){
+            return res.status(400).send({message : "freelancer topilmadi"})
+        }
+        
+        return res.status(200).send(freelancers)
 
     } catch (error) {
         errorHandler(error, res)
@@ -47,7 +77,44 @@ const createFreelancer = async (req, res) => {
 }
 
 
+const updateFreelancer = async (req, res) => {
+    try {
+        
+        const id = req.params.id
+
+        const freelancer = await Freelancer.update({...req.body},{where : {id}})
+        
+        if(!freelancer?.dataValues){
+            return res.status(204).send({message : "freelancer topilmadi"})
+        }
+        
+        return res.status(200).send(freelancer)
+
+    } catch (error) {
+        errorHandler(error, res)
+    }
+}
+
+
+const deleteFreelancer = async (req, res) => {
+    try {
+        
+        const id = req.params.id
+
+        await Freelancer.destroy({where : {id}})
+        
+        return res.status(200).send({message :"deleted"})
+
+    } catch (error) {
+        errorHandler(error, res)
+    }
+}
+
 
 module.exports = {
-    createFreelancer
+    createFreelancer,
+    updateFreelancer,
+    getFreelancers,
+    getFreelancerById,
+    deleteFreelancer
 }
--- router/freelancer.routes.js
+++ router/freelancer.routes.js
@@ -1,9 +1,13 @@
-const { createFreelancer } = require("../controller/freelancer.controller")
+const { createFreelancer, getFreelancers, getFreelancerById, updateFreelancer, deleteFreelancer } = require("../controller/freelancer.controller")
 
 const router = require("express").Router()
 
 
 router.post('/', createFreelancer)
+router.get('/', getFreelancers)
+router.get('/:id', getFreelancerById)
+router.put('/:id', updateFreelancer)
+router.delete('/:id', deleteFreelancer )
 
 
 module.exports  = router
